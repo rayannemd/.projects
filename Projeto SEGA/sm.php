42009b7df7abe64090d13b0750ecc67c61115c23
Rayanne Melo Dias <102603196+rayannemd@users.noreply.github.com>
2022-12-12 19:33:16 -0300

    Add files via upload

---
+++ Projeto SEGA/sm.php
@@ -0,0 +1,26 @@
+<?php include "conexoes/conexaobd.php"?>
+<?php include "conexoes/menu.php"?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <title>Sobre o projeto</title>
+</head>
+<body id="body">
+    
+    <img src="https://www.google.com/url?sa=i&url=https%3A%2F%2Fdoity.com.br%2Fsega2017&psig=AOvVaw2-caobEa7D-sGIEaZUgFAo&ust=1652760852735000&source=images&cd=vfe&ved=0CAkQjRxqFwoTCLiX6Y6U4_cCFQAAAAAdAAAAABAD">
+    <div class="row" id="titulo">
+    <h3 id="titulocor">Super Exposição de Games de Amostras</h3>
+    </div>
+    
+    <br>
+    <p>Visando mostrar na escola as atividades que os alunos do curso técnico de informática da EEEP Manoel Mano, foi realizada pela primeira vez no dia 12 de Agosto de 2017, a 1ª SEGA – Super Exibição de Games e Aplicações da EEEP Manoel Mano. Os alunos do Ensino Médio que participaram do projeto, na disciplina de Programação Orientada a Objetos e Laboratório de Software, neste ano apresentarão em grupos, os sistemas, os jogos e objetos de aprendizagens  que foram trabalhados no projeto, dentre eles temos: jogos de história, química, português, matemática entre outros, sistemas comerciais, além do campeonato de games para os participantes.</p>
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+</body>
+</html>
